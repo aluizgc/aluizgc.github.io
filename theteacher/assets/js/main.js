ac3808d7980257d39986a3971bebebd8f694bf6a
aluizgc <aluizgc@gmail.com>
2021-05-07 15:54:15 -0300

    updates the teacher

--- theteacher/assets/js/main.js
+++ theteacher/assets/js/main.js
@@ -12,14 +12,39 @@ const showMenu = (toggleId, navId) =>{
 showMenu('nav-toggle','nav-menu')
 
 /*===== REMOVE MENU MOBILE =====*/
+const navLink = document.querySelectorAll('.nav__link')
 
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
 
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 /*===== CHANGE BACKGROUND HEADER =====*/ 
-
-
+function scrollHeader(){
+    const header = document.getElementById('header')
+    if(this.scrollY >= 200) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 /*===== SHOW SCROLL TOP =====*/ 
 
 
